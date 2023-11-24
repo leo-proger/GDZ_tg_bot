c8f455e13dd93fb1b989b8dcdb7078d9fea3c302
Leo Proger <leonidkrasavin4@gmail.com>
2023-11-24 17:22:21 +0300

    Добавлен выбор предмета и учебника отдельно

--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-from config import SUBJECTS, HEADERS
+from config import HEADERS
 
 
-def parse_gdz(url: str):
+def parse_gdz(url: str) -> list[str] | int:
 	request = requests.get(url, headers=HEADERS)
 	if request.status_code == 404:
 		return 404
@@ -12,28 +12,24 @@ def parse_gdz(url: str):
 	soup = BeautifulSoup(request.text, 'html.parser')
 
 	# Url фото с решениями
-	exercise_solutions = ['https:' + div.img['src'] for div in soup.find_all('div', class_='with-overtask')]
+	exercise_solutions: list[str] = ['https:' + div.img['src'] for div in soup.find_all('div', class_='with-overtask')]
 
 	return exercise_solutions
 
 
-def get_solve(data: dict, pages: bool = True):
+def get_solve(subject: str, textbook_series: str, page: int, pages: bool = True) -> dict:
 	if pages:
-		subject = data.get('subject')
-		if subject == SUBJECTS['with_pages'].get('english'):
-			page = data.get('page')
+		if subject.lower() == 'английский':
+			url = rf'https://gdz.ru/class-10/english/reshebnik-spotlight-10-afanaseva-o-v/{page}-s/'
 
-			url = fr'https://gdz.ru/class-10/english/reshebnik-spotlight-10-afanaseva-o-v/{page}-s/'
-
-			# В атрибуте src неполная ссылку
 			parsed_solutions = parse_gdz(url)
-			if parsed_solutions != 404:
-				title = f"{SUBJECTS['with_pages'].get('english')} страница {page}"
+			if parsed_solutions and parsed_solutions != 404:
+				title = f"Английский {textbook_series} страница {page}"
 
 				return {'solutions': parsed_solutions, 'title': title, 'status_code': 200}
 			else:
 				return {'status_code': 404}
-
+	return {'status_code': 500}
 
 # dct = {
 # 	'subject': SUBJECTS['with_pages'].get('english'),
