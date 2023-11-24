755610012e50d3657e567165faa78743cf874e4b
Leo Proger <leonidkrasavin4@gmail.com>
2023-11-24 13:43:27 +0300

    Добавлены HEADERS в config.py

--- config.py
+++ config.py
@@ -7,9 +7,13 @@ load_dotenv()
 TOKEN: str = os.environ.get('BOT_TOKEN')
 
 # Предмет, тип (учебник, рабочая тетрадь и тд), класс, авторы
-SUBJECTS: dict[str: str] = {
+SUBJECTS: dict[str: dict[str: str]] = {
 	'with_pages':
 		{
 			'english': 'Английский Spotlight 10 класс Эванс',
 			}
+	}
+
+HEADERS = {
+	'User-Agent': 'Mozilla/5.0',
 	}
