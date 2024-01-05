6db1bdb272606c16ada4ace5ed9c7d3a2cd867e5
Leo_Proger <leonidkrasavin4@gmail.com>
2024-01-05 12:24:00 +0300

    Обновлены сообщения, которые отправляет бот и обработка команд перенесена в main_handler.py

--- app/handlers/main_handler.py
+++ app/handlers/main_handler.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 
 from ..dialogs import *
 from ..dialogs import main_dialog
@@ -17,6 +18,26 @@ router.include_routers(
 	)
 
 
+@router.message(Command('start'))
+async def greeting(message: Message) -> None:
+	await message.answer(config.GREETING_MESSAGE.format(first_name=(message.from_user.first_name or ''),
+	                                                    last_name=(message.from_user.last_name or ''))
+	                     )
+
+
 @router.message(Command('list'))
 async def book_selection(message: Message, dialog_manager: DialogManager) -> None:
 	await dialog_manager.start(MainForm.book)
+
+
+@router.message(Command('help'))
+async def get_help(message: Message) -> None:
+	await message.answer(config.GET_HELP_MESSAGE)
+
+
+
+
+@router.message()
+async def other(message: Message, state: FSMContext) -> None:
+	await state.clear()
+	await message.answer('Чтобы отобразить список учебников, введите /list')
